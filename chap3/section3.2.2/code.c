0d697a4029c9e9552b1d89becd1184b15705572a
jianghao.zhang <jianghaozhang@qq.com>
2020-07-04 14:09:50 +0800

    3.2.2 disassembly

--- chap3/section3.2.2/code.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int accnum = 0;
-
-int sum(int x,int y){
-    int t = x +y;
-    accnum += t;
-    return t;
-}
--- chap3/section3.2.2/main.c
+++ chap3/section3.2.2/main.c
@@ -1,6 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
+
+void multstore(long, long, long *);
 
 int main(){
-    printf("%d",sum(4,5));
-    printf("\n");
+    long d;
+	multstore(2, 4, &d);// transfer the address of d
+	printf("2 * 3 --> %ld\n", d);
+	return 0;
+}
+
+long mult2(long a, long b){
+     long s = a *b;
+	 return s;
 }
